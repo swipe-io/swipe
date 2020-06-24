394a7f6a527df6461f2eb2f4b996999f4054e1e9
vitaly <vitaly@lobchuk.ru>
2020-06-24 14:27:52 +0300

    refactor(gen):

--- fixtures/service/interface.go
+++ fixtures/service/interface.go
@@ -26,8 +26,12 @@ func (*ErrUnauthorized) ErrorCode() int {
 }
 
 type Interface interface {
+	// Create new item of item.
 	Create(ctx context.Context, name string, data []byte) (err error)
+	// Get item.
 	Get(ctx context.Context, id int, name, fname string, price float32, n int) (data user.User, err error)
+	// GetAll more comment and more and more comment and more and more comment and more.
+	// New line comment.
 	GetAll(ctx context.Context) ([]*user.User, error)
 	Delete(ctx context.Context, id uint) (a string, b string, err error)
 	TestMethod(data map[string]interface{}, ss interface{}) (states map[string]map[int][]string, err error)
