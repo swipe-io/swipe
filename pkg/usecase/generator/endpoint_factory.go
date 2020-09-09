0c490370db5e75ceb2f6b0a3e36ad4476dbfdec9
vitaly <vitaly@lobchuk.ru>
2020-09-09 16:40:52 +0300

    feat: Added Path property to EndpointFactory for concatenated to server URL.

--- pkg/usecase/generator/endpoint_factory.go
+++ pkg/usecase/generator/endpoint_factory.go
@@ -23,14 +23,19 @@ func (g *endpointFactory) Prepare(ctx context.Context) error {
 func (g *endpointFactory) Process(ctx context.Context) error {
 	g.W("type EndpointFactory struct{\n")
 	g.W("Options []%sClientOption\n", g.o.ID)
+	g.W("Path string\n")
 	g.W("}\n\n")
 
 	if len(g.o.Methods) > 0 {
 		kitEndpointPkg := g.i.Import("endpoint", "github.com/go-kit/kit/endpoint")
 		ioPkg := g.i.Import("io", "io")
+		stringsPkg := g.i.Import("strings", "strings")
 
 		for _, m := range g.o.Methods {
 			g.W("func (f *EndpointFactory) %sEndpointFactory(instance string) (%s.Endpoint, %s.Closer, error) {\n", m.Name, kitEndpointPkg, ioPkg)
+			g.W("if f.Path != \"\"{\n")
+			g.W("instance = %[1]s.TrimRight(instance, \"/\") + \"/\" + %[1]s.TrimLeft(f.Path, \"/\")", stringsPkg)
+			g.W("}\n")
 			g.W("s, err := NewClient%s%s(instance, f.Options...)\n", g.o.Transport.Prefix, g.o.ID)
 			g.WriteCheckErr(func() {
 				g.W("return nil, nil, err\n")
