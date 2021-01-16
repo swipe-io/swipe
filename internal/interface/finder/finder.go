e083c0f51c3773fa660a64c4b79a52b962dfd06f
Vitaly Lobchuk <vitaly@lobchuk.ru>
2021-01-16 13:10:09 +0300

    refactor: remove gateway

--- internal/interface/finder/finder.go
+++ internal/interface/finder/finder.go
@@ -1,50 +1,45 @@
 package finder
 
-import (
-	"go/build"
-	stdtypes "go/types"
-	"path/filepath"
-	stdstrings "strings"
-
-	"github.com/swipe-io/swipe/v2/internal/domain/model"
-
-	ig "github.com/swipe-io/swipe/v2/internal/interface/gateway"
-
-	"github.com/swipe-io/swipe/v2/internal/option"
-
-	"github.com/swipe-io/swipe/v2/internal/usecase/finder"
-	"github.com/swipe-io/swipe/v2/internal/usecase/gateway"
-)
-
-type serviceFinder struct {
-	loader *option.Loader
-}
-
-func (s *serviceFinder) Find(named *stdtypes.Named) (gateway.ServiceGateway, *model.ServiceInterface, []error) {
-	pkgPathParts := stdstrings.Split(named.Obj().Pkg().Path(), "/")
-	servicePath := filepath.Join(build.Default.GOPATH, "src", stdstrings.Join(pkgPathParts[:3], "/"))
-
-	o, errs := s.loader.Load(servicePath, nil, []string{"./..."})
-	if len(errs) > 0 {
-		return nil, nil, errs
-	}
-	for _, resultOption := range o.Options {
-		if resultOption.Option.Name == "Service" {
-			sg, err := ig.NewServiceGateway(resultOption.Pkg, resultOption.Option, o.Data.GraphTypes, o.Data.CommentFuncs)
-			if err != nil {
-				return nil, nil, []error{err}
-			}
-			for i := 0; i < sg.Interfaces().Len(); i++ {
-				iface := sg.Interfaces().At(i)
-				if iface.TypeName().Obj().String() == named.Obj().String() && sg.JSONRPCEnable() {
-					return sg, iface, nil
-				}
-			}
-		}
-	}
-	return nil, nil, nil
-}
-
-func NewServiceFinder(loader *option.Loader) finder.ServiceFinder {
-	return &serviceFinder{loader: loader}
-}
+//import (
+//	stdtypes "go/types"
+//
+//	"github.com/swipe-io/swipe/v2/internal/domain/model"
+//
+//	"github.com/swipe-io/swipe/v2/internal/option"
+//
+//	"github.com/swipe-io/swipe/v2/internal/usecase/finder"
+//	"github.com/swipe-io/swipe/v2/internal/usecase/gateway"
+//)
+//
+//type serviceFinder struct {
+//	loader *option.Loader
+//}
+//
+//func (s *serviceFinder) Find(named *stdtypes.Named) (gateway.ServiceGateway, *model.ServiceInterface, []error) {
+//	//pkgPathParts := stdstrings.Split(named.Obj().Pkg().Path(), "/")
+//	//servicePath := filepath.Join(build.Default.GOPATH, "src", stdstrings.Join(pkgPathParts[:3], "/"))
+//	//
+//	//o, errs := s.loader.Load(servicePath, nil, []string{"./..."})
+//	//if len(errs) > 0 {
+//	//	return nil, nil, errs
+//	//}
+//	//for _, resultOption := range o.Options {
+//	//	if resultOption.Option.Name == "Service" {
+//	//		sg, err := ig.NewServiceGateway(resultOption.Pkg, resultOption.Option, o.Data.GraphTypes, o.Data.CommentFuncs)
+//	//		if err != nil {
+//	//			return nil, nil, []error{err}
+//	//		}
+//	//		for i := 0; i < sg.Interfaces().Len(); i++ {
+//	//			iface := sg.Interfaces().At(i)
+//	//			if iface.TypeName().Obj().String() == named.Obj().String() {
+//	//				return sg, iface, nil
+//	//			}
+//	//		}
+//	//	}
+//	//}
+//	return nil, nil, nil
+//}
+//
+//func NewServiceFinder(loader *option.Loader) finder.ServiceFinder {
+//	return &serviceFinder{loader: loader}
+//}
