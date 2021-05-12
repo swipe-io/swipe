b55e23eb9927364d0a2649fb1b918838069036ae
Vitaly Lobchuk <vitaly@lobchuk.ru>
2021-05-12 20:35:16 +0300

    refactor: fixed async

--- v2/internal/interface/executor/generation.go
+++ v2/internal/interface/executor/generation.go
@@ -5,7 +5,6 @@ import (
 
 	"fmt"
 	"path/filepath"
-	"sync"
 
 	"github.com/swipe-io/swipe/v2/internal/errors"
 	"github.com/swipe-io/swipe/v2/internal/importer"
@@ -30,63 +29,56 @@ type generationExecutor struct {
 	optionLoader     *option.Loader
 }
 
-func (e *generationExecutor) processGenerate(pkg *packages.Package, generators []generator.Generator) <-chan executor.GenerateResult {
-	outCh := make(chan executor.GenerateResult)
+func (e *generationExecutor) processGenerate(pkg *packages.Package, generators []generator.Generator) (result []executor.GenerateResult) {
 
-	go func() {
-		var wg sync.WaitGroup
-		for _, g := range generators {
-			wg.Add(1)
-			go func(g generator.Generator) {
-				defer wg.Done()
+	for _, g := range generators {
 
-				generated := executor.GenerateResult{}
+		generated := executor.GenerateResult{}
 
-				defer func() {
-					outCh <- generated
-				}()
-
-				if err := g.Prepare(context.TODO()); err != nil {
-					generated.Errs = append(generated.Errs, err)
-					return
-				}
-
-				outputDir := g.OutputDir()
-				if outputDir == "" {
-					basePath, err := types.DetectBasePath(pkg)
-					if err != nil {
-						generated.Errs = append(generated.Errs, err)
-						return
-					}
-					outputDir = basePath
-				}
-
-				generated.PkgPath = pkg.PkgPath
-				generated.OutputPath = filepath.Join(outputDir, g.Filename())
-
-				newImporter := e.importerFactory.NewImporter(generated.OutputPath, pkg)
-				if g, ok := g.(importerer); ok {
-					g.SetImporter(newImporter)
-				}
-
-				if err := g.Process(context.TODO()); err != nil {
-					generated.Errs = append(generated.Errs, err)
-					return
-				}
-				fr := e.frameFactory.NewFrame(generated.OutputPath, newImporter, pkg)
-				content, err := fr.Frame(g.Bytes())
-				if err != nil {
-					generated.Content = g.Bytes()
-					generated.Errs = append(generated.Errs, err)
-					return
-				}
-				generated.Content = content
-			}(g)
+		if err := g.Prepare(context.TODO()); err != nil {
+			generated.Errs = append(generated.Errs, err)
+			result = append(result, generated)
+			continue
 		}
-		wg.Wait()
-		close(outCh)
-	}()
-	return outCh
+
+		outputDir := g.OutputDir()
+		if outputDir == "" {
+			basePath, err := types.DetectBasePath(pkg)
+			if err != nil {
+				generated.Errs = append(generated.Errs, err)
+				result = append(result, generated)
+				continue
+			}
+			outputDir = basePath
+		}
+
+		generated.PkgPath = pkg.PkgPath
+		generated.OutputPath = filepath.Join(outputDir, g.Filename())
+
+		newImporter := e.importerFactory.NewImporter(generated.OutputPath, pkg)
+		if g, ok := g.(importerer); ok {
+			g.SetImporter(newImporter)
+		}
+
+		if err := g.Process(context.TODO()); err != nil {
+			generated.Errs = append(generated.Errs, err)
+			result = append(result, generated)
+			continue
+		}
+		fr := e.frameFactory.NewFrame(generated.OutputPath, newImporter, pkg)
+		content, err := fr.Frame(g.Bytes())
+		if err != nil {
+			generated.Content = g.Bytes()
+			generated.Errs = append(generated.Errs, err)
+			result = append(result, generated)
+			continue
+		}
+		generated.Content = content
+
+		result = append(result, generated)
+	}
+
+	return
 }
 
 func (e *generationExecutor) Execute() (results []executor.GenerateResult, errs []error) {
@@ -97,7 +89,7 @@ func (e *generationExecutor) Execute() (results []executor.GenerateResult, errs 
 	if len(opr.Options) == 0 {
 		return nil, []error{fmt.Errorf("swipe options not found")}
 	}
-	var wg sync.WaitGroup
+
 	for _, o := range opr.Options {
 		fn, ok := e.processorFactory.Get(o.Option.Name)
 		if !ok {
@@ -110,16 +102,11 @@ func (e *generationExecutor) Execute() (results []executor.GenerateResult, errs 
 			errs = append(errs, err)
 			continue
 		}
-		wg.Add(1)
-		go func(p processor.Processor, pkg *packages.Package) {
-			defer wg.Done()
-			outCh := e.processGenerate(pkg, p.Generators(pkg, opr.Data.WorkDir))
-			for generateResult := range outCh {
-				results = append(results, generateResult)
-			}
-		}(p, o.Pkg)
+		processResults := e.processGenerate(o.Pkg, p.Generators(o.Pkg, opr.Data.WorkDir))
+
+		results = append(results, processResults...)
+
 	}
-	wg.Wait()
 	return
 }
 
